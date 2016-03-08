766f1400e9398f8c2b4284c819f5b258a799be68
Chris <ignatz@gmail.com>
2016-03-07 21:36:34 -0500

    Added syllable flags

--- lib/fuzzy_octo_train/syllable.rb
+++ lib/fuzzy_octo_train/syllable.rb
@@ -25,9 +25,14 @@ class Syllable
   def initialize(arg)
     @is_prefix = false
     @is_suffix = false
+    @next_syllable_must_start_with_vowel = false
+    @next_syllable_must_start_with_consonant = false
+    @previous_syllable_must_end_with_vowel = false
+    @previous_syllable_must_end_with_consonant = false
 
     args = arg.to_s.split(' ')
     parse_syllable(args[0])
+    parse_flags(args[1..-1])
   end
 
   def prefix?
@@ -38,18 +43,48 @@ class Syllable
     @is_suffix
   end
 
+  def next_syllable_must_start_with_vowel?
+    @next_syllable_must_start_with_vowel
+  end
+
+  def next_syllable_must_start_with_consonant?
+    @next_syllable_must_start_with_consonant
+  end
+
+  def previous_syllable_must_end_with_vowel?
+    @previous_syllable_must_end_with_vowel
+  end
+
+  def previous_syllable_must_end_with_consonant?
+    @previous_syllable_must_end_with_consonant
+  end
+
   private
 
   def parse_syllable(syll)
-    first_letter = syll[0]
-    if first_letter.eql?('-')
+    captures = /([+-]?)(.+)/.match(syll).captures
+    parse_prefix(captures[0])
+    @syllable = captures[1]
+  end
+
+  def parse_prefix(prefix)
+    if prefix.eql?('-')
       @is_prefix = true
-      @syllable = syll[1..-1]
-    elsif first_letter.eql?('+')
+    elsif prefix.eql?('+')
       @is_suffix = true
-      @syllable = syll[1..-1]
-    else
-      @syllable = syll
+    end
+  end
+
+  def parse_flags(flags)
+    if flags.include?('+v')
+      @next_syllable_must_start_with_vowel = true
+    elsif flags.include?('+c')
+      @next_syllable_must_start_with_consonant = true
+    end
+    if flags.include?('-v')
+      @previous_syllable_must_end_with_vowel = true
+    elsif flags.include?('-c')
+      @previous_syllable_must_end_with_consonant = true
     end
   end
 end
--- test/fuzzy_octo_train/test_syllable.rb
+++ test/fuzzy_octo_train/test_syllable.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require 'fuzzy_octo_train/syllable'
 
 describe Syllable do
-  describe 'considering is_prefix?' do
+  describe 'considering prefix?' do
     describe 'when marked as a prefix' do
       ['-an', '-ang +v', '-ansr +v', '-cael', '-dae +c'].each do |s|
         it 'is_prefix? must return true' do
@@ -20,7 +20,7 @@ describe Syllable do
     end
   end
 
-  describe 'considering is_suffix?' do
+  describe 'considering suffix?' do
     describe 'when marked as a suffix' do
       ['+ath', '+ess', '+san', '+yth', '+las', '+lian', '+evar'].each do |s|
         it 'is_suffix? must return true' do
@@ -33,6 +33,74 @@ describe Syllable do
       ['-que -v +c', 'ria', 'rail', 'ther', 'thus', 'thi', 'san', 'ael -c'].each do |s|
         it 'is_suffix? must return false' do
           refute Syllable.new(s).suffix?
+        end
+      end
+    end
+  end
+
+  describe 'considering next_syllable_must_start_with_vowel?' do
+    describe 'when marked true (+v)' do
+      ['-ang +v', '-ansr +v -c'].each do |s|
+        it 'next_syllable_must_start_with_vowel? must return true' do
+          assert Syllable.new(s).next_syllable_must_start_with_vowel?
+        end
+      end
+    end
+    describe 'when marked false' do
+      ['-ang -v', '-ansr -v +c', '-yada'].each do |s|
+        it 'next_syllable_must_start_with_vowel? must return false' do
+          refute Syllable.new(s).next_syllable_must_start_with_vowel?
+        end
+      end
+    end
+  end
+
+  describe 'considering next_syllable_must_start_with_consonant?' do
+    describe 'when marked true (+c)' do
+      ['-ang +c', '-ansr -v +c'].each do |s|
+        it 'next_syllable_must_start_with_consonant? must return true' do
+          assert Syllable.new(s).next_syllable_must_start_with_consonant?
+        end
+      end
+    end
+    describe 'when marked false' do
+      ['-ang -v', '-ansr -v -c', '-yada'].each do |s|
+        it 'next_syllable_must_start_with_consonant? must return false' do
+          refute Syllable.new(s).next_syllable_must_start_with_consonant?
+        end
+      end
+    end
+  end
+
+  describe 'considering previous_syllable_must_end_with_vowel?' do
+    describe 'when marked true (-v)' do
+      ['-ang -v', '-ansr -v +c'].each do |s|
+        it 'previous_syllable_must_end_with_vowel? must return true' do
+          assert Syllable.new(s).previous_syllable_must_end_with_vowel?
+        end
+      end
+    end
+    describe 'when marked false' do
+      ['-ang +v', '-ansr +v -c', '-yada'].each do |s|
+        it 'previous_syllable_must_end_with_vowel? must return false' do
+          refute Syllable.new(s).previous_syllable_must_end_with_vowel?
+        end
+      end
+    end
+  end
+
+  describe 'considering previous_syllable_must_end_with_consonant?' do
+    describe 'when marked true (-v)' do
+      ['-ang -c', '-ansr +v -c'].each do |s|
+        it 'previous_syllable_must_end_with_consonant? must return true' do
+          assert Syllable.new(s).previous_syllable_must_end_with_consonant?
+        end
+      end
+    end
+    describe 'when marked false' do
+      ['-ang +v', '-ansr -v +c', '-yada'].each do |s|
+        it 'previous_syllable_must_end_with_consonant? must return false' do
+          refute Syllable.new(s).previous_syllable_must_end_with_consonant?
         end
       end
     end
